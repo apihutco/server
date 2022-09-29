03fecd915fd8bc513791350a21344f64dcf33c26
Northes <jieqiang.tan@renrengame.com>
2022-09-29 21:10:50 +0800

    feat(ip): Local source init

---
+++ logic/ip.local.go
@@ -0,0 +1,23 @@
+package logic
+
+import (
+	"apihut-server/dao/mysql"
+	"apihut-server/logic/consts"
+	"apihut-server/models"
+	"net"
+)
+
+type local struct {
+}
+
+func LocalInit() IIPCtrl {
+	return &local{}
+}
+
+func (l *local) GetIP(ip net.IP) (*models.IPBank, error) {
+	return mysql.GetIPBank(ip)
+}
+
+func (l *local) Platform() consts.PlatformID {
+	return consts.Local
+}
