19259b09b951106c1ee2f4f462552be6372dd700
northes <me@northes.co>
2022-11-02 19:16:04 +0800

    feat(response/error): Add error code

--- response/code.go
+++ response/code.go
@@ -10,12 +10,34 @@ type Code uint64
 
 const (
 	CodeSuccess Code = 2000
-	CodeError   Code = 5000
+	CodeError   Code = 5000 + iota
+
+	ErrorFormat
+	ErrorBind
+
+	// IP定位
+	ErrorIPUnableToLocate
+	// 一句招呼
+
+	// 哈希头像
+	ErrorAvatarGenerate
+
+	// 协议测试
+	ErrorProtocolReadBody
+	ErrorProtocolUnmarshal
+	ErrorProtocolWsUpgrade
 )
 
 var CodeMsgMap = map[Code]string{
-	CodeSuccess: "成功",
-	CodeError:   "失败",
+	CodeSuccess:            "成功",
+	CodeError:              "失败",
+	ErrorFormat:            "格式错误",
+	ErrorBind:              "参数绑定失败",
+	ErrorIPUnableToLocate:  "IP无法定位",
+	ErrorAvatarGenerate:    "头像生成失败",
+	ErrorProtocolReadBody:  "读取Body失败",
+	ErrorProtocolUnmarshal: "序列化失败",
+	ErrorProtocolWsUpgrade: "协议升级失败",
 }
 
 func (c Code) Msg() string {
@@ -38,19 +60,23 @@ func Error(c *gin.Context) {
 	ErrorWithMsg(c, CodeError.Msg())
 }
 
+func ErrorWithCode(c *gin.Context, code Code) {
+	JSON(c, code, code.Msg(), nil)
+}
+
 func ErrorWithMsg(c *gin.Context, msg string) {
 	code := CodeError
 	JSON(c, code, msg, nil)
 }
 
-func ErrorWithMsgAndData(c *gin.Context, msg string, data interface{}) {
-	code := CodeError
-	JSON(c, code, msg, data)
-}
-
 func ErrorWithData(c *gin.Context, data interface{}) {
 	code := CodeError
 	JSON(c, code, code.Msg(), data)
+}
+
+func ErrorWithMsgAndData(c *gin.Context, msg string, data interface{}) {
+	code := CodeError
+	JSON(c, code, msg, data)
 }
 
 func JSON(c *gin.Context, code Code, msg string, data interface{}) {
