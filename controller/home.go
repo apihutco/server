8325c025ecedc56eca7321c70cf839c99add3383
Northes <me@northes.co>
2022-11-15 22:31:54 +0800

    feat(controller/home): Better tips

--- controller/home.go
+++ controller/home.go
@@ -1,17 +1,23 @@
 package controller
 
 import (
+	"fmt"
 	"net/http"
+
+	"apihut-server/config"
 
 	"github.com/gin-gonic/gin"
 )
 
 func HomeHandler(c *gin.Context) {
-	c.String(http.StatusOK, "APIHut")
+	msg := fmt.Sprintf("APIHut.\nDocs: %s", config.Share.Site.DocsUrl)
+	c.String(http.StatusOK, msg)
 }
 
 func NotFound(c *gin.Context) {
-	c.String(http.StatusOK, "Not found")
+	path := c.Request.URL.Path
+	msg := fmt.Sprintf("API Not Found. \nYou path is: %s , check it again please. \nOur docs: %s", path, config.Share.Site.DocsUrl)
+	c.String(http.StatusOK, msg)
 }
 
 func HealthHandler(c *gin.Context) {
