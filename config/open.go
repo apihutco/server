fbddf79e24282ed39a3580f53e74067aa172d443
northes <me@northes.io>
2023-06-15 16:34:09 +0800

    feat(config): 新增和风天气开放平台配置

--- config/open.go
+++ config/open.go
@@ -1,14 +1,22 @@
 package config
 
 type Open struct {
-	*Tencent `mapstructure:"tencent"`
-	*Gaode   `mapstructure:"gaode"`
+	*Tencent  `mapstructure:"tencent"`
+	*Gaode    `mapstructure:"gaode"`
+	*QWeather `mapstructure:"qweather"`
 }
 
+// Tencent 腾讯位置服务
 type Tencent struct {
 	Key string
 }
 
+// Gaode 高德开放平台
 type Gaode struct {
+	Key string
+}
+
+// QWeather 和风天气
+type QWeather struct {
 	Key string
 }
